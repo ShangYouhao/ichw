eb08a183762830b0ec3303f843dacb77b51e79e0
ShangYouhao <1800011714@pku.edu.cn>
2018-12-15 22:44:11 +0800

    assign 3

--- pyassign3/tile.py
+++ pyassign3/tile.py
@@ -22,7 +22,6 @@ def judge(m, n, a, b, i):
 def put(m, n, a, b, i=0):
     """能否从i位置开始将剩余部分铺满，若铺满则返回答案组成的列表，否则返回空列表"""
     ans = []
-    cantput = True
     if i >= m*n:
         return [[]]
 
@@ -47,9 +46,38 @@ def put(m, n, a, b, i=0):
     return ans
 
 
+def put_square(m, n, a, i=0):
+    """简化处理正方形的情况"""
+    ans = []
+    if i >= m*n:
+        return [[]]
+
+    while states[i] != 0:
+        i += 1
+        if i == m*n:
+            return [[]]
+
+    if judge(m ,n, a, a, i):
+        for p in range(a):
+            states[i + p*m : i + p*m + a] = [1] * a
+        parts = put_square(m, n, a, i)
+        bricks = tuple([brick for q in range(a)
+                                    for brick in range(i + q * m , i + q * m + a)])
+        for part in parts:
+            part.append(bricks)
+        ans.extend(parts)
+        for p in range(a):
+            states[i + p*m : i + p*m + a] = [0] * a
+
+    return ans
+
+
 def tile(m, n, a, b):
     """除重版本的铺砖函数"""
-    anss = list(set([tuple(ans) for ans in put(m, n, a, b)]))
+    if a == b:
+        anss = list(set([tuple(ans) for ans in put_square(m, n, a)]))
+    else:
+        anss = list(set([tuple(ans) for ans in put(m, n, a, b)]))
     return anss
 
 
@@ -59,7 +87,7 @@ def draw(ans, m, n):
     s0 = int(600 / max(m, n))
     t = turtle.Turtle()
     t.hideturtle()
-    t.speed(10)
+    t.speed(0)
     t.color("blue")
     for i in range(n + 1):
         t.pu()
@@ -113,22 +141,33 @@ def main(m, n, a, b):
         print("不能铺满")
     else:
         print("共有", len(anss), "组解")
-        area = input("""请输入需要查看的解(如：要查看从第35到第42组解，输入"35 42")""").split()
-        if len(area) == 1:
-            print("->", area[0], ":", anss[int(area[0]) - 1])
-        else:
-            while True:
-                (start, end) = (int((area)[0]) - 1, int((area)[1]))
-                if start >= end:
-                    print("输入错误！")
-                    area = input("""请输入需要查看的解(如：要查看从第35到第42个解，输入"35 42")""").split()
-                elif end <= len(anss) and start >= 0:
-                    for (i, ans) in enumerate(anss[start : end]):
-                        print("->", i + start + 1, ":", ans)
-                    break
+        area = input("""请输入需要查看的解(如：要查看从第35到第42组解/第6组解，输入"35 42"/"6")""").split()
+        while True:
+            try:
+                if len(area) == 1:
+                    if int(area[0]) < 1 or int(area[0]) > len(anss):
+                        print("序号超出范围！")
+                        area = input("""请输入需要查看的解(如：要查看从第35到第42个解，输入"35 42")""").split()
+                    else:
+                        print("->", area[0], ":", anss[int(area[0]) - 1])
+                        break
+
                 else:
-                    print("序号超出范围！")
-                    area = input("""请输入需要查看的解(如：要查看从第35到第42个解，输入"35 42")""").split()
+                    (start, end) = (int((area)[0]) - 1, int((area)[1]))
+                    if start >= end:
+                        print("输入错误！")
+                        area = input("""请输入需要查看的解(如：要查看从第35到第42个解，输入"35 42")""").split()
+                    elif end <= len(anss) and start >= 0:
+                        for (i, ans) in enumerate(anss[start : end]):
+                            print("->", i + start + 1, ":", ans)
+                        break
+                    else:
+                        print("序号超出范围！")
+                        area = input("""请输入需要查看的解(如：要查看从第35到第42个解，输入"35 42")""").split()
+
+            except:
+                print("输入错误")
+                area = input("""请输入需要查看的解(如：要查看从第35到第42个解，输入"35 42")""").split()
 
         n_ans = int(input("请选择要画出的解的序号："))
 
